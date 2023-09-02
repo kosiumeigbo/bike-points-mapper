e813a80a8fa5ed317fed0090e5616d46e87b4c0e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-02 16:40:14 +0100

    remove script.js and change to script.ts

--- script.js
+++
@@ -1,9 +0,0 @@
-window.document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://api.tfl.gov.uk/BikePoint/")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data[1]);
-      return fetch(`https://api.tfl.gov.uk/Occupancy/BikePoints/${data[1].id}`).then((res) => res.json());
-    })
-    .then((data) => console.log(data));
-});
