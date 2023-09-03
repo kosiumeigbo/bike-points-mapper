f0fb640f99d6e3e057cb9bfa4582d160da0f8316
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-03 18:23:47 +0100

    add emitted js files

--- js/scripts/script.js
+++ js/scripts/script.js
@@ -1,11 +1,16 @@
 "use strict";
-// window.document.addEventListener("DOMContentLoaded", function () {
+Object.defineProperty(exports, "__esModule", { value: true });
+const module_1 = require("../modules/module");
+console.log(module_1.ted);
 fetch("https://api.tfl.gov.uk/BikePoint/")
     .then((res) => res.json())
     .then((data) => {
     console.log(`length of array is ${data.length}`);
     return fetch(`https://api.tfl.gov.uk/Occupancy/BikePoints/${data[1].id}`).then((res) => res.json());
 })
-    .then((data) => console.log(data))
+    .then((data) => {
+    const [pointData] = data;
+    console.log(pointData);
+    document.body.textContent = `${pointData.id}`;
+})
     .catch((err) => console.error(err));
-// });
