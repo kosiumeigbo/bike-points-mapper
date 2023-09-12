fde5ae1b553978e76e8e4dbbe59fb57d9975df51
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-12 20:03:24 +0100

    update script.ts file

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,24 +1,8 @@
-// window.document.addEventListener("DOMContentLoaded", function () {
+import {getDataForAllBikePoints} from "../modules/module";
 
-import {ArrayOfBikePoints} from "../modules/module";
-/*
-fetch("https://api.tfl.gov.uk/BikePoint/")
+const bikePointsURL: string = "https://api.tfl.gov.uk/BikePoint/";
+getDataForAllBikePoints(bikePointsURL).then((data) => console.log(data));
+
+fetch("https://api.tfl.gov.uk/Journey/Meta/Modes")
   .then((res) => res.json())
-  .then((data) => {
-    console.log(data[1]);
-    return fetch(
-      `https://api.tfl.gov.uk/Occupancy/BikePoints/${data[1].id}`
-    ).then((res) => res.json());
-  })
-  .then((data) => {
-    const [pointData] = data;
-    console.log(pointData);
-    document.body.textContent = `${pointData.id}`;
-  })
-  .catch((err) => console.error(err));
-*/
-// });
-
-ArrayOfBikePoints("https://api.tfl.gov.uk/BikePoint/").then((data) =>
-  console.log(data)
-);
+  .then((data) => console.log(data));
