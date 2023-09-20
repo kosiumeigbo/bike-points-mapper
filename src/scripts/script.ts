37f23996498d79e903549392bc59bfd2ca3d094e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-20 22:46:04 +0100

    clean up script.ts file

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,62 +1,9 @@
-import {getDataForAllBikePoints} from "../modules/module";
-import {BikePoint} from "../modules/types";
-import Chart from "chart.js/auto";
+import {buildPage} from "../modules/module";
 
 const bikePointsURL: string = "https://api.tfl.gov.uk/BikePoint/";
 
-const barChartArea = document.getElementById("bar-chart");
-const pieChartArea = document.getElementById("pie-chart");
-
-const barChartConfigObj = {
-  type: "bar",
-};
-const pieChartConfigObj = {
-  type: "pie",
-};
-
-(async function () {
-  try {
-    const sortedBikePoints = (await getDataForAllBikePoints(bikePointsURL)) as {
-      name: string;
-      bikePoints: BikePoint[];
-    }[];
-
-    if (sortedBikePoints instanceof Error) {
-      throw new Error("Could not get data from server");
-    }
-
-    const barChartDataObj = {
-      labels: sortedBikePoints.map((area) => area.name),
-      datasets: [
-        {
-          label: "Number of bikepoints",
-          data: sortedBikePoints.map((area) => area.bikePoints.length),
-        },
-      ],
-    };
-
-    const pieChartDataObj = {
-      labels: sortedBikePoints.map((area) => area.name),
-      datasets: [
-        {
-          label: "Number of bikepoints",
-          data: sortedBikePoints.map((area) => area.bikePoints.length),
-        },
-      ],
-    };
-
-    if (barChartArea && pieChartArea) {
-      new Chart(barChartArea, {
-        ...barChartConfigObj,
-        data: barChartDataObj,
-      });
-
-      new Chart(pieChartArea, {
-        ...pieChartConfigObj,
-        data: pieChartDataObj,
-      });
-    }
-  } catch (e) {
-    console.log(e);
-  }
-})();
+window.addEventListener("DOMContentLoaded", function () {
+  buildPage(bikePointsURL).then((mainSection) =>
+    document.body.insertAdjacentElement("beforeend", mainSection as HTMLElement)
+  );
+});
