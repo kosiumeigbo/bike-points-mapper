2fdf57fc1ab12ecdc2d8a5790ba81dacfe2ba6e3
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2023-09-23 13:10:05 +0100

    testing something

--- src/modules/module.ts
+++ src/modules/module.ts
@@ -156,6 +156,7 @@ export const buildPage = async function (url: string) {
       throw sortedBikePointsArray;
     }
     console.log(sortedBikePointsArray);
+    console.log(sortedBikePointsArray);
 
     // Main page content container
     const main = document.createElement("main");
